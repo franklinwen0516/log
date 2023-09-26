2d2b5d39cdea28b77fa6902fee2b48020ea2d47e
franklinwen <ruiming.wen@ackings.net>
2023-09-26 15:24:38 -0700

    init

---
+++ logger_test.go
@@ -0,0 +1,243 @@
+package log
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func _zeroTime() time.Time {
+	return time.Time{}
+}
+
+func TestLogger(t *testing.T) {
+	t.Parallel()
+
+	w := io.Discard
+	l := New(w)
+	for i := 0; i < 100; i++ {
+		t.Run("race", func(t *testing.T) {
+			t.Parallel()
+			s := l.StandardLog()
+			l.Info("foo")
+			l.GetLevel()
+			l.Print("foo")
+
+			s.Print("foo")
+			s.Writer().Write([]byte("bar"))
+			s.Output(1, "baz")
+
+			l.SetOutput(w)
+			l.Debug("foo")
+			l.SetLevel(InfoLevel)
+			l.GetPrefix()
+
+			o := l.With("foo", "bar")
+			o.Print("foo")
+			o.SetTimeFormat(time.Kitchen)
+			o.Debug("foo")
+			o.SetOutput(w)
+			o.Error("foo")
+			o.SetFormatter(JSONFormatter)
+		})
+	}
+}
+func TestPrint(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(FatalLevel)
+	SetTimeFunction(_zeroTime)
+	SetReportTimestamp(true)
+	SetReportCaller(false)
+	SetTimeFormat(DefaultTimeFormat)
+	Error("error")
+	Print("print")
+	assert.Equal(t, "0001/01/01 00:00:00 print\n", buf.String())
+}
+
+func TestPrintf(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(FatalLevel)
+	SetTimeFunction(_zeroTime)
+	SetReportTimestamp(true)
+	SetReportCaller(false)
+	SetTimeFormat(DefaultTimeFormat)
+	Errorf("error")
+	Printf("print")
+	assert.Equal(t, "0001/01/01 00:00:00 print\n", buf.String())
+}
+
+func TestFatal(t *testing.T) {
+	SetReportTimestamp(true)
+	SetReportCaller(false)
+	SetTimeFormat(DefaultTimeFormat)
+	if os.Getenv("FATAL") == "1" {
+		Fatal("i'm dead")
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestFatal")
+	cmd.Env = append(os.Environ(), "FATAL=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
+}
+
+func TestFatalf(t *testing.T) {
+	SetReportTimestamp(true)
+	SetReportCaller(false)
+	SetTimeFormat(DefaultTimeFormat)
+	if os.Getenv("FATAL") == "1" {
+		Fatalf("i'm %s", "dead")
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestFatalf")
+	cmd.Env = append(os.Environ(), "FATAL=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
+}
+
+func TestDebugf(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(DebugLevel)
+	SetTimeFunction(_zeroTime)
+	SetReportTimestamp(true)
+	SetReportCaller(true)
+	SetTimeFormat(DefaultTimeFormat)
+	_, file, line, _ := runtime.Caller(0)
+	Debugf("debug %s", "foo")
+	assert.Equal(t, fmt.Sprintf("0001/01/01 00:00:00 DEBU <log/%s:%d> debug foo\n", filepath.Base(file), line+1), buf.String())
+}
+
+func TestInfof(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(InfoLevel)
+	SetReportTimestamp(false)
+	SetReportCaller(false)
+	SetTimeFormat(DefaultTimeFormat)
+	Infof("info %s", "foo")
+	assert.Equal(t, "INFO info foo\n", buf.String())
+}
+
+func TestWarnf(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(WarnLevel)
+	SetReportCaller(false)
+	SetReportTimestamp(true)
+	SetTimeFunction(_zeroTime)
+	SetTimeFormat(DefaultTimeFormat)
+	Warnf("warn %s", "foo")
+	assert.Equal(t, "0001/01/01 00:00:00 WARN warn foo\n", buf.String())
+}
+
+func TestErrorf(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(ErrorLevel)
+	SetReportCaller(false)
+	SetReportTimestamp(true)
+	SetTimeFunction(_zeroTime)
+	SetTimeFormat(time.Kitchen)
+	Errorf("error %s", "foo")
+	assert.Equal(t, "12:00AM ERRO error foo\n", buf.String())
+}
+
+func TestWith(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(InfoLevel)
+	SetReportCaller(false)
+	SetReportTimestamp(true)
+	SetTimeFunction(_zeroTime)
+	SetTimeFormat(DefaultTimeFormat)
+	With("foo", "bar").Info("info")
+	assert.Equal(t, "0001/01/01 00:00:00 INFO info foo=bar\n", buf.String())
+}
+
+func TestGetLevel(t *testing.T) {
+	SetLevel(InfoLevel)
+	assert.Equal(t, InfoLevel, GetLevel())
+}
+
+func TestPrefix(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(InfoLevel)
+	SetReportCaller(false)
+	SetReportTimestamp(false)
+	SetPrefix("prefix")
+	Info("info")
+	assert.Equal(t, "INFO prefix: info\n", buf.String())
+	assert.Equal(t, "prefix", GetPrefix())
+	SetPrefix("")
+}
+
+func TestFormatter(t *testing.T) {
+	var buf bytes.Buffer
+	SetOutput(&buf)
+	SetLevel(InfoLevel)
+	SetReportCaller(false)
+	SetReportTimestamp(false)
+	SetFormatter(JSONFormatter)
+	Info("info")
+	assert.Equal(t, "{\"lvl\":\"info\",\"msg\":\"info\"}\n", buf.String())
+}
+
+func TestWithPrefix(t *testing.T) {
+	l := WithPrefix("test")
+	assert.Equal(t, "test", l.prefix)
+}
+
+func TestApplication(t *testing.T) {
+
+	Error("This is error level error")
+	Warn("This is warn level error")
+	Info("This is info level error")
+	Debug("This is debug level error")
+	SetLevel(FatalLevel)
+	//log.Fatal("This is fatal level error")
+	Error("This is error level error")
+	Warn("This is warn level error")
+	Info("This is info level error")
+	Debug("This is debug level error")
+	SetLevel(ErrorLevel)
+	//log.Fatal("This is fatal level error")
+	Error("This is error level error")
+	Warn("This is warn level error")
+	Info("This is info level error")
+	Debug("This is debug level error")
+	SetLevel(WarnLevel)
+	//log.Fatal("This is fatal level error")
+	Error("This is error level error")
+	Warn("This is warn level error")
+	Info("This is info level error")
+	Debug("This is debug level error")
+	SetLevel(InfoLevel)
+	//log.Fatal("This is fatal level error")
+	Error("This is error level error")
+	Warn("This is warn level error")
+	Info("This is info level error")
+	Debug("This is debug level error")
+	SetLevel(DebugLevel)
+	//log.Fatal("This is fatal level error")
+	Error("This is error level error")
+	Warn("This is warn level error")
+	Info("This is info level error")
+	Debug("This is debug level error")
+}
